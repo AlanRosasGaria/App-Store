264e4a783d3c74cf42b7b573014f29477d13744a
GerardoMa12 <115763158+GerardoMa12@users.noreply.github.com>
2022-12-08 14:31:05 -0600

    Update products.js

--- src/assets/fake-data/products.js
+++ src/assets/fake-data/products.js
@@ -59,7 +59,7 @@ const products = [
         image01: product_02_image_01,
         image02: product_02_image_02,
         categorySlug: "sudadera",
-        colors: ["negro"],
+        colors: ["negro"], ["blanco"], ["verde"], ["azul"],
         slug: "ao-thun-dinosaur-02",
         size: ["s", "m"],
         description: `
@@ -75,7 +75,7 @@ const products = [
         image01: product_03_image_01,
         image02: product_03_image_02,
         categorySlug: "playeras",
-        colors: ["azul"],
+        colors: ["azul"], ["blanco"], ["verde"], ["negro"],
         slug: "ao-thun-dinosaur-03",
         size: ["m"],
         description: `
@@ -94,7 +94,7 @@ const products = [
         image01: product_04_image_01,
         image02: product_04_image_02,
         categorySlug: "playeras",
-        colors: ["blanco"],
+        colors: ["blanco"], ["azul"], ["verde"], ["negro"],
         slug: "ao-thun-polo-04",
         size: ["xl"],
         description: `
@@ -113,7 +113,7 @@ const products = [
         image01: product_05_image_01,
         image02: product_05_image_02,
         categorySlug: "sudadera",
-        colors: ["azul", "verde"],
+        colors: ["azul"], ["verde"], ["blanco"], ["negro"], 
         slug: "ao-thun-polo-05",
         size: ["xxl"],
         description: `
@@ -132,7 +132,7 @@ const products = [
         image01: product_06_image_01,
         image02: product_06_image_02,
         categorySlug: "sudadera",
-        colors: ["negro"],
+        colors: ["negro"], ["blanco"], ["verde"], ["azul"],
         slug: "ao-thun-polo-06",
         size: ["s", "m", "xl"],
         description: `
@@ -151,7 +151,7 @@ const products = [
         image01: product_07_image_01,
         image02: product_07_image_02,
         categorySlug: "sueter",
-        colors: ["negro"],
+        colors: ["negro"], ["verde"], ["azul"], ["blanco"],
         slug: "ao-somi-caro-07",
         size: ["l", "xl"],
         description: `
@@ -170,7 +170,7 @@ const products = [
         image01: product_08_image_01,
         image02: product_08_image_02,
         categorySlug: "sudadera",
-        colors: ["negro"],
+        colors: ["negro"], ["verde"], ["azul"], ["blanco"],
         slug: "ao-somi-dai-tay-08",
         size: ["s", "m", "xl"],
         description: `
@@ -189,7 +189,7 @@ const products = [
         image01: product_09_image_01,
         image02: product_09_image_02,
         categorySlug: "sueter",
-        colors: ["negro", "blanco"],
+        colors: ["negro"], ["blanco"], ["azul"], ["verde"],
         slug: "ao-somi-tay-dai-09",
         size: ["m"],
         description: `
@@ -208,10 +208,10 @@ const products = [
         image01: product_10_image_01,
         image02: product_10_image_02,
         categorySlug: "quan-jean",
-        colors: ["blue", "black"],
+        colors: ["azul"], ["negro"], ["blanco"], ["verde"],
         slug: "quan-jean-phong-cach-10",
         size: ["l"],
-        description: "Sự hiện diện của những chiếc áo thun basic cổ tròn trong tủ đồ của bạn chính là chìa khóa giúp cho bạn có thêm nhiều outfit thú vị mà lại không cần đến quá nhiều món đồ. Áo thun nữ cotton cổ tròn basic chính là vũ khí tiện dụng cho các chị em trong trang phục hàng ngày!<br><br><br>Thiết kế đơn giản, form dáng tiện lợi của áo thun PPN4502. Tại sao chỉ với 1 chiếc áo thun nữ basic mà bạn có thể phối với 10 bộ độ khác nhau? Câu trả lời nằm ở chính sự đơn giản của chúng. Càng đơn giản, bạn lại càng dễ mix & match với những món đồ khác nhau.Áo thun nữ PPM4502 có thiết kế cổ tròn đơn giản, nhẹ nhàng tôn da. Tay cáo, form áo cũng không hề cầu kỳ, rất dễ mặc với nhiều thân hình khác nhau. Đặc biệt hơn, màu sắc của chiếc áo phông nữ cổ tròn này cũng rất nhã nhặn, trung tính, trơn màu. Sự tối giản từ thiết kế, đường may đến bảng màu giúp các chị em không cần đắn đo quá nhiều khi lựa chọn. Chất liệu cotton 95% được xử lý nghiêm ngặt, quy trình và công nghệ hiện đại nên mang tới cho chiếc áo sự thoải mái, mềm mại, thoáng mát ngay khi chạm vào. Cùng với đó, áo thun nữ cotton cổ tròn Yody có khả năng thâm shuts mồ hôi rất tốt nên người mặc không bị cảm giác bí bách, dính dính trên da khi đổ mồ hôi vào mùa hè. Bên cạnh đó, sản phẩm cũng chưa 5% spandex - loại sợi giúp co giãn, đàn hồi hiệu quả thích hợp mặc tới nhiều môi trường, ngay cả khi vận động <br><br><br> Sự hiện diện của những chiếc áo thun basic cổ tròn trong tủ đồ của bạn chính là chìa khóa giúp cho bạn có thêm nhiều outfit thú vị mà lại không cần đến quá nhiều món đồ. Áo thun nữ cotton cổ tròn basic chính là vũ khí tiện dụng cho các chị em trong trang phục hàng ngày!<br><br><br>Thiết kế đơn giản, form dáng tiện lợi của áo thun PPN4502. Tại sao chỉ với 1 chiếc áo thun nữ basic mà bạn có thể phối với 10 bộ độ khác nhau? Câu trả lời nằm ở chính sự đơn giản của chúng. Càng đơn giản, bạn lại càng dễ mix & match với những món đồ khác nhau.Áo thun nữ PPM4502 có thiết kế cổ tròn đơn giản, nhẹ nhàng tôn da. Tay cáo, form áo cũng không hề cầu kỳ, rất dễ mặc với nhiều thân hình khác nhau. Đặc biệt hơn, màu sắc của chiếc áo phông nữ cổ tròn này cũng rất nhã nhặn, trung tính, trơn màu. Sự tối giản từ thiết kế, đường may đến bảng màu giúp các chị em không cần đắn đo quá nhiều khi lựa chọn. Chất liệu cotton 95% được xử lý nghiêm ngặt, quy trình và công nghệ hiện đại nên mang tới cho chiếc áo sự thoải mái, mềm mại, thoáng mát ngay khi chạm vào. Cùng với đó, áo thun nữ cotton cổ tròn Yody có khả năng thâm shuts mồ hôi rất tốt nên người mặc không bị cảm giác bí bách, dính dính trên da khi đổ mồ hôi vào mùa hè. Bên cạnh đó, sản phẩm cũng chưa 5% spandex - loại sợi giúp co giãn, đàn hồi hiệu quả thích hợp mặc tới nhiều môi trường, ngay cả khi vận động"
+        description:  
     },
     {
         title: "Quần jean 11",
@@ -219,7 +219,7 @@ const products = [
         image01: product_11_image_01,
         image02: product_11_image_02,
         categorySlug: "quan-jean",
-        colors: ["blue", "black"],
+        colors: ["azul"], ["negro"],
         slug: "quan-jean-11",
         size: ["s", "m", "xl"],
         description: "Sự hiện diện của những chiếc áo thun basic cổ tròn trong tủ đồ của bạn chính là chìa khóa giúp cho bạn có thêm nhiều outfit thú vị mà lại không cần đến quá nhiều món đồ. Áo thun nữ cotton cổ tròn basic chính là vũ khí tiện dụng cho các chị em trong trang phục hàng ngày!<br><br><br>Thiết kế đơn giản, form dáng tiện lợi của áo thun PPN4502. Tại sao chỉ với 1 chiếc áo thun nữ basic mà bạn có thể phối với 10 bộ độ khác nhau? Câu trả lời nằm ở chính sự đơn giản của chúng. Càng đơn giản, bạn lại càng dễ mix & match với những món đồ khác nhau.Áo thun nữ PPM4502 có thiết kế cổ tròn đơn giản, nhẹ nhàng tôn da. Tay cáo, form áo cũng không hề cầu kỳ, rất dễ mặc với nhiều thân hình khác nhau. Đặc biệt hơn, màu sắc của chiếc áo phông nữ cổ tròn này cũng rất nhã nhặn, trung tính, trơn màu. Sự tối giản từ thiết kế, đường may đến bảng màu giúp các chị em không cần đắn đo quá nhiều khi lựa chọn. Chất liệu cotton 95% được xử lý nghiêm ngặt, quy trình và công nghệ hiện đại nên mang tới cho chiếc áo sự thoải mái, mềm mại, thoáng mát ngay khi chạm vào. Cùng với đó, áo thun nữ cotton cổ tròn Yody có khả năng thâm shuts mồ hôi rất tốt nên người mặc không bị cảm giác bí bách, dính dính trên da khi đổ mồ hôi vào mùa hè. Bên cạnh đó, sản phẩm cũng chưa 5% spandex - loại sợi giúp co giãn, đàn hồi hiệu quả thích hợp mặc tới nhiều môi trường, ngay cả khi vận động <br><br><br> Sự hiện diện của những chiếc áo thun basic cổ tròn trong tủ đồ của bạn chính là chìa khóa giúp cho bạn có thêm nhiều outfit thú vị mà lại không cần đến quá nhiều món đồ. Áo thun nữ cotton cổ tròn basic chính là vũ khí tiện dụng cho các chị em trong trang phục hàng ngày!<br><br><br>Thiết kế đơn giản, form dáng tiện lợi của áo thun PPN4502. Tại sao chỉ với 1 chiếc áo thun nữ basic mà bạn có thể phối với 10 bộ độ khác nhau? Câu trả lời nằm ở chính sự đơn giản của chúng. Càng đơn giản, bạn lại càng dễ mix & match với những món đồ khác nhau.Áo thun nữ PPM4502 có thiết kế cổ tròn đơn giản, nhẹ nhàng tôn da. Tay cáo, form áo cũng không hề cầu kỳ, rất dễ mặc với nhiều thân hình khác nhau. Đặc biệt hơn, màu sắc của chiếc áo phông nữ cổ tròn này cũng rất nhã nhặn, trung tính, trơn màu. Sự tối giản từ thiết kế, đường may đến bảng màu giúp các chị em không cần đắn đo quá nhiều khi lựa chọn. Chất liệu cotton 95% được xử lý nghiêm ngặt, quy trình và công nghệ hiện đại nên mang tới cho chiếc áo sự thoải mái, mềm mại, thoáng mát ngay khi chạm vào. Cùng với đó, áo thun nữ cotton cổ tròn Yody có khả năng thâm shuts mồ hôi rất tốt nên người mặc không bị cảm giác bí bách, dính dính trên da khi đổ mồ hôi vào mùa hè. Bên cạnh đó, sản phẩm cũng chưa 5% spandex - loại sợi giúp co giãn, đàn hồi hiệu quả thích hợp mặc tới nhiều môi trường, ngay cả khi vận động"
